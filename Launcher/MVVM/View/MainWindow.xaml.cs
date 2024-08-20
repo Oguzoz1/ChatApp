54bf8c55275e45577ddc19e8bab2d9c2eb536240
Oguz Ozuer <oguzozuer@gmail.com>
2024-08-20 14:09:25 +0300

    +Close Button and visual enhancement

--- Launcher/MVVM/View/MainWindow.xaml.cs
+++ Launcher/MVVM/View/MainWindow.xaml.cs
@@ -54,5 +54,10 @@ namespace LauncherClient
                 }
             }
         }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
     }
 }
